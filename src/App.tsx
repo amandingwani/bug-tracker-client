813f3b8fc2a275210f76c6aa0700b9c564106405
Aman Dingwani <amandingwani@gmail.com>
2023-10-03 21:54:37 +0530

    feat: Add google oauth2 basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+// import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleGoogleAuthResponse = (response: any) => {
+    console.log("Encoded JWT ID token: " + response.credential);
+  };
+
+  useEffect(() => {
+    // @ts-ignore
+    google.accounts.id.initialize({
+      client_id: import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID,
+      callback: handleGoogleAuthResponse,
+    });
+
+    // @ts-ignore
+    google.accounts.id.renderButton(
+      document.getElementById("googleSignInButton"),
+      { theme: "outline", size: "large" }  // customization attributes
+    );
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>HMR testing</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id="googleSignInButton" ></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
