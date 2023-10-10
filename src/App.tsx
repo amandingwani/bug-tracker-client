f564d4843def57c199de7c3a2f9e1aff4f94b4c6
Aman Dingwani <amandingwani@gmail.com>
2023-10-11 01:54:42 +0530

    fix: accessing google object before google script is loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // import "./App.css";
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
+
   const handleGoogleAuthResponse = (response: any) => {
     console.log("Encoded JWT ID token: " + response.credential);
   };
 
   useEffect(() => {
+    if (!loaded) return;
+    // Script tag is loaded!
     // @ts-ignore
     google.accounts.id.initialize({
       client_id: import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID,
@@ -18,6 +22,13 @@ function App() {
       document.getElementById("googleSignInButton"),
       { theme: "outline", size: "large" }  // customization attributes
     );
+  }, [loaded]);
+
+  useEffect(() => {
+    const scriptTag = document.createElement('script');
+    scriptTag.src = 'https://accounts.google.com/gsi/client';
+    scriptTag.addEventListener('load', () => setLoaded(true));
+    document.body.appendChild(scriptTag);
   }, []);
 
   return (
