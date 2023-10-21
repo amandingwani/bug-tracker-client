262596fd92beb006ff886057b241c0afa4ebf5ac
Aman Dingwani <amandingwani@gmail.com>
2023-10-21 11:35:22 +0530

    temporary commit, adding mui design

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,76 @@
-import { useEffect, useState } from 'react';
-import axios from './config/axios';
-import { GOOGLE_OAUTH_CLIENT_ID } from './config/env';
+import 'src/global.css';
 
-function App() {
-  const [loaded, setLoaded] = useState(false);
+import { useScrollToTop } from './hooks/use-scroll-to-top';
 
-  const handleGoogleAuthResponse = async (response: any) => {
-    console.log('Encoded JWT ID token: ' + response.credential);
-    try {
-      const { data } = await axios.post('/auth/login', {
-        token: response.credential,
-      });
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+import Router from './routes/sections';
+import ThemeProvider from './theme';
 
-  useEffect(() => {
-    if (!loaded) return;
-    // Script tag is loaded!
-    // @ts-ignore
-    google.accounts.id.initialize({
-      client_id: GOOGLE_OAUTH_CLIENT_ID,
-      callback: handleGoogleAuthResponse,
-    });
+// import { useEffect, useState } from 'react';
+// import axios from './config/axios';
+// import { GOOGLE_OAUTH_CLIENT_ID } from './config/env';
+// import Button from '@mui/material/Button';
 
-    // @ts-ignore
-    google.accounts.id.renderButton(
-      document.getElementById('googleSignInButton'),
-      { theme: 'outline', size: 'large' } // customization attributes
-    );
-  }, [loaded]);
+// ----------------------------------------------------------------------
 
-  useEffect(() => {
-    const scriptTag = document.createElement('script');
-    scriptTag.src = 'https://accounts.google.com/gsi/client';
-    scriptTag.addEventListener('load', () => setLoaded(true));
-    document.body.appendChild(scriptTag);
-  }, []);
+export default function App() {
+  useScrollToTop();
 
-  async function getHealth() {
-    console.log('pressed');
-    try {
-      const res = await axios.get('http://localhost:3000/health');
-      console.log(res);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  return <ThemeProvider>{/* <Router /> */}</ThemeProvider>;
 
-  return (
-    <>
-      <div id="googleSignInButton"></div>
-      <button onClick={getHealth}>Get backend Health</button>
-    </>
-  );
+  // const [loaded, setLoaded] = useState(false);
+
+  // const handleGoogleAuthResponse = async (response: any) => {
+  //   console.log('Encoded JWT ID token: ' + response.credential);
+  //   try {
+  //     const { data } = await axios.post('/auth/login', {
+  //       token: response.credential,
+  //     });
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   if (!loaded) return;
+  //   // Script tag is loaded!
+  //   // @ts-ignore
+  //   google.accounts.id.initialize({
+  //     client_id: GOOGLE_OAUTH_CLIENT_ID,
+  //     callback: handleGoogleAuthResponse,
+  //   });
+
+  //   // @ts-ignore
+  //   google.accounts.id.renderButton(
+  //     document.getElementById('googleSignInButton'),
+  //     { theme: 'outline', size: 'large' } // customization attributes
+  //   );
+  // }, [loaded]);
+
+  // useEffect(() => {
+  //   const scriptTag = document.createElement('script');
+  //   scriptTag.src = 'https://accounts.google.com/gsi/client';
+  //   scriptTag.addEventListener('load', () => setLoaded(true));
+  //   document.body.appendChild(scriptTag);
+  // }, []);
+
+  // async function getHealth() {
+  //   console.log('pressed');
+  //   try {
+  //     const res = await axios.get('http://localhost:3000/health');
+  //     console.log(res);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // return (
+  //   <>
+  //     <div id="googleSignInButton"></div>
+  //     <Button variant="contained" onClick={getHealth}>
+  //       Get backend Health
+  //     </Button>
+  //     {/* <ThemeProvider></ThemeProvider> */}
+  //   </>
+  // );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-// import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HelmetProvider>
+    <BrowserRouter>
+      <Suspense>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Suspense>
+    </BrowserRouter>
+  </HelmetProvider>
 );
