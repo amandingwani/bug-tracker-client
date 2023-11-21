3c15b3545cb911f50e51967a5f0241431cadf84d
Aman Dingwani <amandingwani@gmail.com>
2023-11-21 20:53:20 +0530

    feat: Add "Bug Ninja" after logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -2,7 +2,8 @@ import { forwardRef } from 'react';
 
 import Box from '@mui/material/Box';
 import Link, { LinkProps } from '@mui/material/Link';
-import { SxProps, Theme, useTheme } from '@mui/material/styles';
+import { SxProps, Theme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 
 import { RouterLink } from 'src/routes/components';
 
@@ -11,41 +12,51 @@ import { RouterLink } from 'src/routes/components';
 interface LogoProps {
   disabledLink?: boolean;
   sx?: SxProps<Theme>;
-  [other: string]: any;
 }
 
-const Logo = forwardRef<HTMLDivElement, LogoProps>(
-  ({ disabledLink = false, sx, ...other }, ref) => {
-    const theme = useTheme();
+// eslint-disable-next-line
+const Logo = forwardRef<HTMLAnchorElement, LogoProps>(({ disabledLink = false, sx }, ref) => {
+  // const theme = useTheme();
 
-    const PRIMARY_LIGHT = theme.palette.primary.light;
+  // const PRIMARY_LIGHT = theme.palette.primary.light;
 
-    const PRIMARY_MAIN = theme.palette.primary.main;
+  // const PRIMARY_MAIN = theme.palette.primary.main;
 
-    const PRIMARY_DARK = theme.palette.primary.dark;
+  // const PRIMARY_DARK = theme.palette.primary.dark;
 
-    // OR using local (public folder)
-    // -------------------------------------------------------
-    const logo = (
+  // OR using local (public folder)
+  // -------------------------------------------------------
+  const logo = (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        cursor: 'pointer',
+        ...sx,
+      }}
+    >
       <Box
         component="img"
         src="/favicon/android-chrome-512x512.png"
-        sx={{ width: 40, height: 40, cursor: 'pointer', ...sx }}
+        sx={{ width: 40, height: 40 }}
       />
-    );
+      <Box component="text" sx={{ ml: 2 }}>
+        <Typography variant="h4">{'Bug Ninja'}</Typography>
+      </Box>
+    </Box>
+  );
 
-    if (disabledLink) {
-      return logo;
-    }
-
-    const linkProps: LinkProps = {
-      component: RouterLink,
-      href: '/',
-      sx: { display: 'contents' },
-    };
-
-    return <Link {...linkProps}>{logo}</Link>;
+  if (disabledLink) {
+    return logo;
   }
-);
+
+  const linkProps: LinkProps = {
+    component: RouterLink,
+    href: '/',
+    sx: { display: 'contents' },
+  };
+
+  return <Link {...linkProps}>{logo}</Link>;
+});
 
 export default Logo;
