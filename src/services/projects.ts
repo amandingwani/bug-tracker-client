288e35e48183c34f954fa3a7904ba02aa57c3d0e
Aman Dingwani <amandingwani@gmail.com>
2023-12-23 21:48:38 +0530

    feat: Add createTicket axios function

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -1,5 +1,5 @@
 import axios from 'src/config/axios';
-import { ProjectCreateInput, ProjectsState, Project } from 'src/redux/types';
+import { ProjectCreateInput, ProjectsState, Project, TicketCreateInput, Ticket } from 'src/redux/types';
 
 export const getProjects = () => {
     return new Promise<ProjectsState>((resolve, reject) => {
@@ -14,6 +14,15 @@ export const createProject = (data: ProjectCreateInput) => {
     return new Promise<Project>((resolve, reject) => {
         axios
             .post('/projects', data)
+            .then((res) => resolve(res.data))
+            .catch((err) => reject(err));
+    });
+}
+
+export const createTicket = (data: TicketCreateInput) => {
+    return new Promise<Ticket>((resolve, reject) => {
+        axios
+            .post('/tickets', data)
             .then((res) => resolve(res.data))
             .catch((err) => reject(err));
     });
