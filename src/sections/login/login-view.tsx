7518f9067319ad7245a110af05b19f218087e850
Aman Dingwani <amandingwani@gmail.com>
2024-01-17 04:12:06 +0530

    feat: Update login view

--- src/sections/login/login-view.tsx
+++ src/sections/login/login-view.tsx
@@ -22,11 +22,17 @@ import Logo from 'src/components/logo';
 import Iconify from 'src/components/iconify';
 import { useAppSelector } from 'src/redux/hooks';
 import { selectReqStatus } from 'src/redux/slices/authSlice';
+import { useResponsive } from 'src/hooks/use-responsive';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
 // ----------------------------------------------------------------------
 
 export default function LoginView() {
   const theme = useTheme();
+
+  const lgUp = useResponsive('up', 'lg');
+  console.log(lgUp);
 
   const router = useRouter();
 
@@ -80,7 +86,169 @@ export default function LoginView() {
   //   </>
   // );
 
-  return (
+  // const renderUpLg = (
+  //   <Box
+  //   // sx={{
+  //   //   ...bgGradient({
+  //   //     color: alpha(theme.palette.background.default, 0.8),
+  //   //     imgUrl: '/assets/background/overlay_4.jpg',
+  //   //   }),
+  //   //   height: 1,
+  //   // }}
+  //   >
+  //     <Stack direction="row">
+  //       <Stack direction="column" width={'30%'}>
+  //         {/* <Stack alignItems="center" justifyContent="center" sx={{ height: 1, px: 2 }}> */}
+  //         <Box
+  //           sx={{
+  //             py: 5,
+  //             px: { xs: 4, sm: 5, md: 5 },
+  //             width: 1,
+  //             maxWidth: 420,
+  //           }}
+  //         >
+  //           <Logo withText={false} />
+  //           <Typography variant="h4" sx={{ mb: 4 }}>
+  //             Welcome to Bug Ninja
+  //           </Typography>
+
+  //           {/* <Typography variant="body2" sx={{ mt: 2, mb: 5 }}>
+  //           Don’t have an account?
+  //           <Link variant="subtitle2" sx={{ ml: 0.5 }}>
+  //             Get started
+  //           </Link>
+  //         </Typography> */}
+
+  //           <Stack direction="row" spacing={2}>
+  //             <LoadingButton
+  //               loading={reqStatus.status === 'loading'}
+  //               onClick={() => handleGoogleLoginClick()}
+  //               fullWidth
+  //               size="large"
+  //               color="inherit"
+  //               variant="outlined"
+  //               startIcon={<Iconify icon="flat-color-icons:google" />}
+  //               sx={{ borderColor: alpha(theme.palette.grey[500], 0.16), fontWeight: 'light' }}
+  //             >
+  //               Continue with Google
+  //             </LoadingButton>
+  //           </Stack>
+
+  //           <Divider sx={{ my: 3 }}>
+  //             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+  //               OR
+  //             </Typography>
+  //           </Divider>
+
+  //           <LoadingButton
+  //             fullWidth
+  //             size="large"
+  //             type="submit"
+  //             variant="contained"
+  //             color="inherit"
+  //             onClick={handleClick}
+  //           >
+  //             Demo Login
+  //           </LoadingButton>
+  //           {/* {renderForm} */}
+  //         </Box>
+  //         {/* </Stack> */}
+  //       </Stack>
+  //       {/* <Box
+  //         component="img"
+  //         sx={{
+  //           height: '100vh',
+  //           width: '70%',
+  //           // maxHeight: { xs: 233, md: 167 },
+  //           // maxWidth: { xs: 350, md: 250 },
+  //         }}
+  //         alt="Bug Ninja welcome image"
+  //         src="/assets/background/overlay_3.jpg"
+  //       /> */}
+  //       <img src="/assets/background/overlay_3.jpg" style={{ float: 'right' }} />
+  //     </Stack>
+  //   </Box>
+  // );
+
+  const renderUpLg = (
+    <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid item xs={12} sm={8} md={4} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            // py: 5,
+            // px: { xs: 4, sm: 5, md: 5 },
+            // width: 1,
+            // maxWidth: 420,
+            my: 10,
+            mx: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+          }}
+        >
+          <Logo withText={false} />
+
+          <Typography variant="h4" sx={{ mt: 12, mb: 5 }}>
+            Welcome to Bug Ninja
+          </Typography>
+
+          {/* <Typography variant="body2" sx={{ mt: 2, mb: 5 }}>
+            Don’t have an account?
+            <Link variant="subtitle2" sx={{ ml: 0.5 }}>
+              Get started
+            </Link>
+          </Typography> */}
+
+          <LoadingButton
+            loading={reqStatus.status === 'loading'}
+            onClick={() => handleGoogleLoginClick()}
+            fullWidth
+            size="large"
+            color="inherit"
+            variant="outlined"
+            startIcon={<Iconify icon="flat-color-icons:google" />}
+            sx={{ borderColor: alpha(theme.palette.grey[500], 0.16), fontWeight: 'light' }}
+          >
+            Continue with Google
+          </LoadingButton>
+
+          <Divider sx={{ my: 3, width: '100%' }}>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              OR
+            </Typography>
+          </Divider>
+
+          <LoadingButton
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            color="inherit"
+            onClick={handleClick}
+          >
+            Demo Login
+          </LoadingButton>
+          {/* {renderForm} */}
+        </Box>
+      </Grid>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={8}
+        sx={{
+          backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: (t) =>
+            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+    </Grid>
+  );
+
+  const renderDownLg = (
     <Box
       sx={{
         ...bgGradient({
@@ -155,4 +323,7 @@ export default function LoginView() {
       </Stack>
     </Box>
   );
+
+  return lgUp ? renderUpLg : renderDownLg;
+  // return renderUpLg;
 }
