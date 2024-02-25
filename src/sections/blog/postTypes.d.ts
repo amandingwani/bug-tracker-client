f0304bc2807412ae281b481ce61eee035311df00
Aman Dingwani <amandingwani@gmail.com>
2024-02-25 13:26:58 +0530

    Refactor: Remove unused old section: blog

--- src/sections/blog/postTypes.d.ts
+++
@@ -1,9 +0,0 @@
-export interface PostOption {
-  value: string;
-  label: string;
-}
-
-export interface PostSortProps {
-  options: Array<PostOption>;
-  onSort?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
-}
