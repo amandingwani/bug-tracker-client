911ed079f0a1c35fec4e9e635fcfb8dd6c743386
Aman Dingwani <amandingwani@gmail.com>
2024-05-16 00:20:39 +0530

    chore: Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,13 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn', // or "error"
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
   },
 };
